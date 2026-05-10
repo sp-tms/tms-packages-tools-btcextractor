06218d4762f4b214e80a2c27d56910850e19d14d
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-10 09:55:59 +0530

    update

--- Btcextractor.php
+++ Btcextractor.php
@@ -6,56 +6,10 @@ use System\Base\BasePackage;
 
 class Btcextractor extends BasePackage
 {
-    //protected $modelToUse = ::class;
-
     protected $packageName = 'btcextractor';
 
-    public $btcextractor;
-
-    public function getBtcextractorById($id)
-    {
-        $btcextractor = $this->getById($id);
-
-        if ($btcextractor) {
-            //
-            $this->addResponse('Success');
-
-            return;
-        }
-
-        $this->addResponse('Error', 1);
-    }
-
-    public function addBtcextractor($data)
+    public function process()
     {
         //
-    }
-
-    public function updateBtcextractor($data)
-    {
-        $btcextractor = $this->getById($id);
-
-        if ($btcextractor) {
-            //
-            $this->addResponse('Success');
-
-            return;
-        }
-
-        $this->addResponse('Error', 1);
-    }
-
-    public function removeBtcextractor($data)
-    {
-        $btcextractor = $this->getById($id);
-
-        if ($btcextractor) {
-            //
-            $this->addResponse('Success');
-
-            return;
-        }
-
-        $this->addResponse('Error', 1);
     }
 }
